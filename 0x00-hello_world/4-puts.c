c0ebd823b501194060441956ac8603a485071daa
John <john.flouis1@gmail.com>
2023-02-23 09:11:11 -0800

    edit putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+/*
+ * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
  *
  * Use the function puts
  * You are not allowed to use printf
