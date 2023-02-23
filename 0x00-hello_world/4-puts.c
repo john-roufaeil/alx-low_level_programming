0e00a13974f7ae1e450544b53e8e3e9f22cb19e4
John <john.flouis1@gmail.com>
2023-02-23 09:15:07 -0800

    add documentation for main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 /*
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- *
+ * Write a C program that prints exactly 
+ * "Programming is like building a multilingual puzzle
+ * , followed by a new line.
  * Use the function puts
  * You are not allowed to use printf
  * Your program should end with the value 0
  */
 #include <stdio.h>
-int main() {
+/**
+ * puts the string!
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
