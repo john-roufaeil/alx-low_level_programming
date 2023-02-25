0d002f5cf2c3e704ce5b57eab82b51a4f25f7749
John <john.flouis1@gmail.com>
2023-02-25 05:49:24 -0800

    comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print digits from 0 to 9 separated by a comma
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
