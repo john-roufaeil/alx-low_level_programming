8d3562eaf8193a38d73b6cec55d48d0a3944cbbc
John <john.flouis1@gmail.com>
2023-02-25 05:35:34 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print digits from 0 to f in hexadecimal
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
