3f59569586f9b6681e162509d536f08539a346ca
John <john.flouis1@gmail.com>
2023-02-27 08:59:27 -0800

    alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i = 0;
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c++);
+		}
+		_putchar('\n');
+	}
+}
