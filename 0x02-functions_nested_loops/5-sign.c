7f6c33953204d3a05af44d49bdcb44b6173a0f0b
John <john.flouis1@gmail.com>
2023-02-28 06:55:24 -0800

    fix 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - prints the sign of an integer
@@ -11,14 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	}
 	if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
 	}
-	printf("-");
+	_putchar('-');
 	return (-1);
 }
