248cf313223e7d4214b68a75bccde5c64b94bfe4
John <john.flouis1@gmail.com>
2023-02-28 09:52:27 -0800

    update main.h

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#define LARGE 10000000000
 
 /**
  * main - prints first 98 fibonacci numbers
@@ -9,14 +10,21 @@
 int main(void)
 {
 	unsigned long int i, a, b, c;
+	unsigned long int hold1, hold2;
 
 	a = 1;
-	b = 1;
-	printf("1");
-	for (i = 1; i < 97; i++)
+	b = 2;
+	printf("1, 2");
+	for (i = 2; i < 98; i++)
 	{
 		printf(", ");
+		if (a + b > LARGE)
+		{
+			hold1 = (a + b) / LARGE;
+			hold2 = (a + b) % LARGE;
+			c = 
 		c = a + b;
+
 		printf("%lu", c);
 		a = b;
 		b = c;
