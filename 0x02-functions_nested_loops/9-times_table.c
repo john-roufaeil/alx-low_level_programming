5a3cb6f9ad1230d2da335fe35d0dac7d04d837bc
John <john.flouis1@gmail.com>
2023-02-28 07:35:59 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,20 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints all minutes of a day
- *
- * Return: 0
+ * times_table - prints the time table from 0 through 9
  */
-void jack_bauer(void)
+void times_table(void)
 {
-	int h0, h1, m0, m1;
+	int op0, op1;
 
-	for (h0 = 0; h0 < 3; h0++)
+	for (op0 = 0; op0 < 9; op0++)
 	{
-		for (h1 = 0; (h0 < 2 && h1 < 10) || (h0 == 2 && h1 < 4); h1++)
+		for (op1 = 0; op1 < 9; op1++)
 		{
-			for (m0 = 0; m0 < 6; m0++)
-			{
-				for (m1 = 0; m1 < 10; m1++)
-				{
-					_putchar(h0);
-					_putchar(h1);
-					_putchar(':');
-					_putchar(m0);
-					_putchar(m1);
-					_putchar('\n');
-				}
-			}
+			_putchar(op0 * op1 + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
+		_putchar('\n');
 	}
 }
