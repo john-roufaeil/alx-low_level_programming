9d05073f310c9a76907959c4441684eeea04a8bc
John <john.flouis1@gmail.com>
2023-02-28 15:28:49 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a string
+ */
+void rev_string(char *s)
+{
+	int a = 0, b = 0;
+	char temp;
+
+	while (!(s[a] == '\0'))
+	{
+		a++;
+	}
+	while (b++ < a--)
+	{
+		temp = s[b];
+		s[b] = s[a];
+		s[a] = temp;
+		_putchar(s[count]);
+	}
+}
