9072521b7a19201a77cd612bfb9b2504994f1ff2
John <john.flouis1@gmail.com>
2023-02-28 07:13:21 -0800

    edit comment

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - computes last digit value of an integer
+ * print_last_digit - computes last digit value of an integer
  * @n: input integer
  *
  * Return: value of last digit of n
