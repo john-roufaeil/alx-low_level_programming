37f3b2f57d2fa09e03f112c109db2252ec4eb7a5
John <john.flouis1@gmail.com>
2023-02-28 16:12:32 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,13 +6,15 @@
  */
 void puts_half(char *str)
 {
-	int i = 0;
+	int i = 0, half = 0, max = 0;
 
 	while (!(str[i] == '\0'))
 	{
 		i++;
 	}
-	for (i /= 2; i >= 0; i--)
+	half = i / 2 + 1;
+	max = i;
+	for (i = half; i < max; i++)
 	{
 		_putchar(str[i]);
 	}
