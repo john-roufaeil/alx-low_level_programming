bc195e6e02a097e6164a3f89ae09a4f984e19c13
John <john.flouis1@gmail.com>
2023-02-28 16:39:40 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi- converts a string to an int
+ * @s: pointer to the string
+ *
+ * Return: 0 if no integer found, the integer otherwise
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int found = 0, minus = 0, result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			minus++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			found = 1;
+		}
+		else if (found == 1)
+			break;
+		i++;
+	}
+	if (found == 0)
+		return (0);
+	if (minus % 2 == 1)
+		return ((-1) * result);
+	return (result);
+}
