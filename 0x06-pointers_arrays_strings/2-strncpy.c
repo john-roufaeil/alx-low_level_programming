497504f7f40733b97bd7751776690fb06a2082ad
John <john.flouis1@gmail.com>
2023-03-02 05:09:38 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string to another until the nth character of src
+ * @dest: string to paste in
+ * @src: string to copy from
+ * @n: at most n bytes to be copied from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
