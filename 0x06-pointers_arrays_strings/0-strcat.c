4b690c15180002b87516281a1da92fd2c72e2279
John <john.flouis1@gmail.com>
2023-03-02 04:16:53 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,8 +9,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	while (*dest++ != '\0');
-	dest--;
-	while (*dest++ = *src++);
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		dest[i + j] = src[j++];
+	dest[i + j] = '\0';
 	return (dest);
 }	
